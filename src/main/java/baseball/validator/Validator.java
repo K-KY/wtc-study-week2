54fb80becf8af4b451390434d351b43be4708ad8
K-KY <kyuyoungk@naver.com>
2023-09-13 22:34:28 +0900

    feat : 모든 검증 메소드를 호출한다

--- src/main/java/baseball/validator/Validator.java
+++ src/main/java/baseball/validator/Validator.java
@@ -4,23 +4,32 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Validator {
+    public void validateNumber(String input) {
+        validateType(input);
+        validateNumberRange(input);
+        validateNumberLength(input);
+        validateNumberDuplicate(input);
+    }
     //숫자가 아닌 값이 있는지 확인한다.
-    public void validateType(String input) {
+    private void validateType(String input) {
         if (!input.matches("(.*)[0-9](.*)")) {
             throw new IllegalArgumentException("숫자를 입력해주세요.");
         }
     }
-    public void validateNumberRange(String input) {
+    //0이 존재하는지 확인한다
+    private void validateNumberRange(String input) {
         if (input.contains("0")) {
             throw new IllegalArgumentException("숫자 범위는 1 ~ 9 입니다");
         }
     }
-    public void validateNumberLength(String input) {
+    //길이가 3인지 확인한다
+    private void validateNumberLength(String input) {
         if (input.length() != 3) {
             throw new IllegalArgumentException("3개의 숫자를 입력해주세요");
         }
     }
-    public void validateNumberDuplicate(String input) {
+    //숫자가 중복되는지 확인한다.
+    private void validateNumberDuplicate(String input) {
         List<String> list = Arrays.asList(input.split(""));
         if (input.length() != list.stream().distinct().count()) {
             throw new IllegalArgumentException("중복되지 않는 숫자를 입력해주세요.");
