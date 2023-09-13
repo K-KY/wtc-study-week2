b5de80db308fa41fba7e1eb32b8534738405e19a
K-KY <kyuyoungk@naver.com>
2023-09-13 22:25:01 +0900

    feat : 숫자가 3개인지 확인한다

--- src/main/java/baseball/validator/Validator.java
+++ src/main/java/baseball/validator/Validator.java
@@ -1,10 +1,18 @@
 package baseball.validator;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Validator {
     //숫자가 아닌 값이 있는지 확인한다.
     public void validateType(String input) {
         if (!input.matches("(.*)[1-9](.*)")) {
             throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
+    }
+    public void validateNumberLength(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3개의 숫자를 입력해주세요");
         }
     }
 
