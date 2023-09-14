a4be1441fdd14edc5fbc6f63a84c5026b9b35fe3
K-KY <kyuyoungk@naver.com>
2023-09-14 20:40:15 +0900

    feat : 사용자가 입력학 숫자를 검증하고 userInputNumber 에 저장

---
+++ src/main/java/baseball/userinput/UserInput.java
@@ -0,0 +1,19 @@
+package baseball.userinput;
+
+import baseball.validator.Validator;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInput {
+    private final List<String> userInputNumber = new ArrayList<>();
+    public void userInput() {
+        Validator validator = new Validator();
+        System.out.print("숫자를 입력해주세요 : ");
+        String userNumber = Console.readLine();
+        System.out.println();
+        userInputNumber.addAll(validator.validateNumber(userNumber));
+    }
+
+}
