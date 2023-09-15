6a4f03c296219cfc16d18d61482fe538db1cd898
K-KY <kyuyoungk@naver.com>
2023-09-16 06:14:36 +0900

    style : 개행 추가

--- src/main/java/baseball/output/ResultOutput.java
+++ src/main/java/baseball/output/ResultOutput.java
@@ -6,16 +6,16 @@ public class ResultOutput {
     public void resultOutput(NumberCompare numberCompare) {
         printBall(numberCompare.ball);
         printStrike(numberCompare.strike);
-
+        System.out.println();
     }
     private void printBall(int ball) {
         if (ball != 0) {
-            System.out.println(ball + "볼 ");
+            System.out.print(ball + "볼 ");
         }
     }
     private void printStrike(int Strike) {
         if (Strike != 0) {
-            System.out.println(Strike + "스트라이크 ");
+            System.out.print(Strike + "스트라이크 ");
         }
     }
 }
