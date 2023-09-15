8a5c9e130f6c7f6982ad62730f7b4778247adbe0
K-KY <kyuyoungk@naver.com>
2023-09-16 05:18:16 +0900

    feat : 볼 결과 출력

--- src/main/java/baseball/output/ResultOutput.java
+++ src/main/java/baseball/output/ResultOutput.java
@@ -4,8 +4,11 @@ import baseball.game.NumberCompare;
 
 public class ResultOutput {
     public void resultOutput(NumberCompare numberCompare) {
-        System.out.println(numberCompare.strike);
-        System.out.println(numberCompare.ball);
-        System.out.println(numberCompare.nothing);
+        printBall(numberCompare.strike);
+    }
+    private void printBall(int ball) {
+        if (ball != 0) {
+            System.out.println(ball + "볼 ");
+        }
     }
 }
