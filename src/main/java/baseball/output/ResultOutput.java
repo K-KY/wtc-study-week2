81f39c0e785a8d17a09751c0fed0aefe534382c9
K-KY <kyuyoungk@naver.com>
2023-09-16 05:18:59 +0900

    feat : 스트라이크 결과 출력

--- src/main/java/baseball/output/ResultOutput.java
+++ src/main/java/baseball/output/ResultOutput.java
@@ -4,11 +4,18 @@ import baseball.game.NumberCompare;
 
 public class ResultOutput {
     public void resultOutput(NumberCompare numberCompare) {
-        printBall(numberCompare.strike);
+        printBall(numberCompare.ball);
+        printStrike(numberCompare.strike);
+
     }
     private void printBall(int ball) {
         if (ball != 0) {
-            System.out.println(ball + "볼 ");
+            System.out.println(ball + "스트라이크 ");
+        }
+    }
+    private void printStrike(int Strike) {
+        if (Strike != 0) {
+            System.out.println(Strike + "스트라이크 ");
         }
     }
 }
