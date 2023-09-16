ef1d9394f180c4a8c9e066cfd4853bd485f8a474
K-KY <kyuyoungk@naver.com>
2023-09-16 12:55:38 +0900

    style : 재귀가 중첩되서 실행되지 않도록 boolean 추가

--- src/main/java/baseball/game/GameController.java
+++ src/main/java/baseball/game/GameController.java
@@ -1,18 +1,32 @@
 package baseball.game;
 
+import baseball.NumberGenerator.NumberGenerator;
+import baseball.output.SystemOutput;
 import baseball.userinput.UserInput;
 
 public class GameController {
     private final NumberCompare numberCompare = new NumberCompare();
+    private final SystemOutput systemOutput = new SystemOutput();
+    private boolean progress = true;
+
     public void gameStart() {
         UserInput userInput = UserInput.getInstance();
         userInput.userInput();
         numberCompare.compare();
-        gameEnd();
+        checkEnd();
     }
-    public void gameEnd() {
+
+    public void checkEnd() {
         if (numberCompare.strike != 3) {
             gameStart();
+        }
+        if (numberCompare.strike == 3 && progress) {
+            progress = false;
+            if (systemOutput.gameEnd()) {
+                progress = true;
+                NumberGenerator.getInstance().reset();
+                gameStart();
+            }
         }
     }
 }
